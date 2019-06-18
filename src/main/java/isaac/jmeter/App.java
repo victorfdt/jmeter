100cc96e790f61cf3e00d3861bdec17df76bd6cd
Victor Di Trani <victor.ditrani@isaac.nl>
2019-06-18 13:54:17 +0200

    initial commit

---
+++ src/main/java/isaac/jmeter/App.java
@@ -0,0 +1,13 @@
+package isaac.jmeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
