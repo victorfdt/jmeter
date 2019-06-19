384a47ff410cc5cecdc30f260b0c0a74891dd5ba
Victor Di Trani <victor.ditrani@isaac.nl>
2019-06-19 10:10:09 +0200

    adding more content in result.jtl

--- src/main/java/isaac/jmeter/App.java
+++ src/main/java/isaac/jmeter/App.java
@@ -1,5 +1,6 @@
 package isaac.jmeter;
 
+
 /**
  * Hello world!
  *
@@ -8,6 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String date = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        
+        System.out.println(date);
     }
 }
